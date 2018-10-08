1d9d1cc782f21159d8a7a7fc636518fe5470c72b
jsdiuf <weichun713@foxmail.com>
2018-10-08 10:58:46 +0800

    update

---
+++ src/main/java/com/jsdiuf/jsdiuf/designPatten/decorator/Circle.java
@@ -0,0 +1,12 @@
+package com.jsdiuf.jsdiuf.designPatten.decorator;
+
+/**
+ * @author weicc
+ * @create 2018-09-29 15:46
+ **/
+public class Circle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Shape: Circle");
+    }
+}
---
+++ src/main/java/com/jsdiuf/jsdiuf/designPatten/decorator/Rectangle.java
@@ -0,0 +1,12 @@
+package com.jsdiuf.jsdiuf.designPatten.decorator;
+
+/**
+ * @author weicc
+ * @create 2018-09-29 15:45
+ **/
+public class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Shape: Rectangle");
+    }
+}
